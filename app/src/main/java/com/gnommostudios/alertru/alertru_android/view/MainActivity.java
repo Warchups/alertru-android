849b5c10f729d06c9a055b9c774273eb4bc6a2fd
ChristianDAM <adolmitoc@hotmail.com>
2018-04-18 11:51:55 +0200

    Correccióon de imports

--- app/src/main/java/com/gnommostudios/alertru/alertru_android/view/MainActivity.java
+++ app/src/main/java/com/gnommostudios/alertru/alertru_android/view/MainActivity.java
@@ -1,7 +1,6 @@
 package com.gnommostudios.alertru.alertru_android.view;
 
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
@@ -13,11 +12,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.gnommostudios.alertru.alertru_android.R;
 import com.gnommostudios.alertru.alertru_android.adapter.AdapterMenu;
