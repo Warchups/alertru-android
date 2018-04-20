dc48caf7674dd73c4939d3255cf7ccc6a88d7d9c
ChristianDAM <christian_sellent@hotmail.com>
2018-04-20 12:28:12 +0200

    .

--- app/src/main/java/com/gnommostudios/alertru/alertru_android/view/fragments/SearchFragment.java
+++ app/src/main/java/com/gnommostudios/alertru/alertru_android/view/fragments/SearchFragment.java
@@ -26,6 +26,7 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
         alertList = (ListView) view.findViewById(R.id.alert_list);
