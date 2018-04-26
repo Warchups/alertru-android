6d3c4349752c0d17ffc784d1b82b6154d8544aa7
ChristianDAM <christian_sellent@hotmail.com>
2018-04-26 10:19:57 +0200

    Modificación notificaciones con preferencias

--- app/src/main/java/com/gnommostudios/alertru/alertru_android/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/gnommostudios/alertru/alertru_android/service/MyFirebaseMessagingService.java
@@ -2,6 +2,7 @@ package com.gnommostudios.alertru.alertru_android.service;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
@@ -14,47 +15,62 @@ import com.gnommostudios.alertru.alertru_android.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private SharedPreferences prefs;
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.i("FIREBASE", remoteMessage.getNotification().getBody());
 
-        long [] vibrate = {0,100,200,300};
-        //Uri sound = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.turtle);
-        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        prefs = getSharedPreferences("preferences", Context.MODE_PRIVATE);
 
-        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.icon_alarma);
+        boolean activateAlert = prefs.getBoolean("activateAlert", true);
 
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.icon_alarma)
-                        .setLargeIcon(image)
-                        .setPriority(Integer.parseInt(remoteMessage.getData().get("priority")))
-                        .setContentTitle(remoteMessage.getNotification().getTitle())
-                        .setContentText(remoteMessage.getNotification().getBody())
-                        .setVibrate(vibrate)
+        if (activateAlert) {
+            String ringote = prefs.getString("ringote", "");
+            Uri sound;
 
-                        .setSound(sound)
-                        .setLights(0xFFFF0000, 300, 100);
-
-
-        Log.i("PRUEBA", remoteMessage.getData().get("priority"));
-        Log.i("TIME", remoteMessage.getTtl()+"");
-
-        final NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        mNotificationManager.notify(0, mBuilder.build());
-
-        long delayInMilliseconds = remoteMessage.getTtl();
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                mNotificationManager.cancelAll();
+            if (ringote.equals("a1")) {
+                sound = RingtoneManager.getDefaultUri(R.raw.alarm1);
+            } else if (ringote.equals("a2")) {
+                sound = RingtoneManager.getDefaultUri(R.raw.alarm2);
+            } else if (ringote.equals("be")) {
+                sound = RingtoneManager.getDefaultUri(R.raw.beep);
+            } else if (ringote.equals("nu")) {
+                sound = RingtoneManager.getDefaultUri(R.raw.nuclear);
+            } else {
+                sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
             }
-        }, delayInMilliseconds);
+
+            long[] vibrate = {0, 100, 200, 300};
+
+            Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.icon_alarma);
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.icon_alarma)
+                            .setLargeIcon(image)
+                            .setPriority(Integer.parseInt(remoteMessage.getData().get("priority")))
+                            .setContentTitle(remoteMessage.getNotification().getTitle())
+                            .setContentText(remoteMessage.getNotification().getBody())
+                            .setVibrate(vibrate)
+                            .setSound(sound)
+                            .setLights(0xFFFF0000, 300, 100);
+
+            Log.i("PRUEBA", remoteMessage.getData().get("priority"));
+            Log.i("TIME", remoteMessage.getTtl() + "");
+
+            final NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            mNotificationManager.notify(0, mBuilder.build());
+
+            long delayInMilliseconds = remoteMessage.getTtl();
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    mNotificationManager.cancelAll();
+                }
+            }, delayInMilliseconds);
+        }
     }
 }
